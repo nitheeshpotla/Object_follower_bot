6725fd7119b82e06d3811debef8ddedb125add96
Nitheesh Potla <156593594+nitheeshpotla@users.noreply.github.com>
2024-12-10 06:11:15 +0530

    Add files via upload

---
+++ learning_log/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+]
